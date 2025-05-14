b90dffb0f961a8bb3ab5f11526d4335371fd279d
Yevhen Arutiunian <arytynyanevgen@gmail.com>
2025-05-14 23:18:20 +0300

    RefactorCode_1

--- src/main/java/com/company/vehicles/Car.java
+++ src/main/java/com/company/vehicles/Car.java
@@ -3,50 +3,57 @@ package com.company.vehicles;
 import com.company.details.Engine;
 import org.example.professions.Driver;
 
-
 import java.util.Scanner;
 
 public class Car {
     Engine engine;
     Driver driver;
 
-        public void displayInfo1() {
+    public static void main(String[] args) {
+        launchCar();
+    }
+    private static void launchCar(){
+        String arg1 = enterArg1();
+        String arg2 = enterArg2();
+        Car car = new Car();
+        car.engine = new Engine(enterEngineType());
+        car.driver = new Driver(enterCarModel());
+        System.out.println(
+                "MachineNumber: " + arg1 +
+                        "\nNameDriver: " + arg2
+        );
+        car.displayInfo2();
+        car.displayInfo1();
+    }
+    private void displayInfo1() {
         driver.displayInfo1();
     }
 
-    public void displayInfo2() {
+    private void displayInfo2() {
         engine.displayInfo2();
     }
 
-
-    public static void main(String[] args) {
-        //ввод модели двигателя
+    private static String enterEngineType() {
         Scanner scanner1 = new Scanner(System.in);
         System.out.print("Введите модель двигателя: ");
-        String userModel = scanner1.nextLine();
-        //ввод модели машины
+        return scanner1.nextLine();
+    }
+
+    private static String enterCarModel() {
         Scanner scanner2 = new Scanner(System.in);
         System.out.print("Введите модель машины: ");
-        String carModel = scanner2.nextLine();
-        //ввод двух аргументов
-        //1
+        return scanner2.nextLine();
+    }
+
+    private static String enterArg1() {
         Scanner scanner3 = new Scanner(System.in);
         System.out.print("Введите номер машины: ");
-        String machineNumber = scanner3.nextLine();
-        //2
+        return scanner3.nextLine();
+    }
+
+    private static String enterArg2() {
         Scanner scanner4 = new Scanner(System.in);
         System.out.print("Введите имя водителя: ");
-        String nameDriver = scanner4.nextLine();
-        //вывод
-        Car car = new Car();
-        car.engine = new Engine(userModel);
-        car.driver = new Driver(carModel);
-        car.displayInfo1();
-        car.displayInfo2();
-        System.out.println(
-                "MachineNumber: " + machineNumber +
-                        "\nNameDriver: " + nameDriver
-        );
-
+        return scanner4.nextLine();
     }
 }
